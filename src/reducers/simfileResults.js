f1d44bc4323615ddba1ce654919bc5e758e3172d
Lewis King <lewisisgood@gmail.com>
2020-04-13 19:20:49 -0500

    Adding redux-form to package.json to help with form submit handling

--- src/reducers/simfileResults.js
+++ src/reducers/simfileResults.js
@@ -1,3 +1,6 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { submitQuery } from '../actions';
+import { reduxForm, Field } from 'redux-form'
 
 // Dummy reducer; does nothing.
 export default (state=[], action) => state;
