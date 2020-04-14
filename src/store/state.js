47297bc07425bf475b5aad38a9aa9f43aeeb379a
Lewis King <lewisisgood@gmail.com>
2020-04-13 19:05:08 -0500

    Adding more songs to the sample state

--- src/store/state.js
+++ src/store/state.js
@@ -1,11 +1,38 @@
 export default {
     simfileResults: [
-        {songName: "My cool song",
-        songArtist: "DJ SNAZZ",
-        bpm: 69,
-        packName: "Foobar",
-        difficulty: {"easy": 3},
-        expanded: false
-    }
+	    {
+		   "songName":"My cool song",
+		   "songArtist":"DJ SNAZZ",
+		   "bpm":69,
+		   "packName":"Foobar",
+		   "difficulty":{
+		      "easy":3
+		   },
+		   "expanded":false
+		},
+		{
+		   "songName":"Freaknik is Bacc",
+		   "songArtist":"T-Pain",
+		   "bpm":420,
+		   "packName":"Rackem",
+		   "difficulty":{
+		      "oni":13
+		   },
+		   "expanded":false
+		},
+		{
+		   "songName":"Thizzle DanCE",
+		   "songArtist":"Mac Dre",
+		   "bpm":111,
+		   "packName":"Hoozawutzit",
+		   "difficulty":{
+		      "beginner":1,
+		      "light":3,
+		      "standard":5,
+		      "heavy":8,
+		      "challenge":11,
+		   },
+		   "expanded":false
+		}
     ]
 };
