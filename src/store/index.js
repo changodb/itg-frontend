d040a01e57144fee30b25c3c7a89f9287a21a3ca
Lewis King <lewis.king@fauna.com>
2020-04-17 14:43:10 -0500

    Adding thunk middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import ReduxThunk from 'redux-thunk';
 import state from './state';
 import rootReducer from '../reducers';
+
+const middleware = [thunk];
 
 export default configureStore({
     reducer: rootReducer,
