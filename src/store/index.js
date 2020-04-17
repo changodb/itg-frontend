7b44b1b34e295ad458af87f94fe16c58b5340131
Lewis King <lewis.king@fauna.com>
2020-04-17 15:35:44 -0500

    Adding devTools and middleware to configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import state from './state';
 import rootReducer from '../reducers';
 
-const middleware = [thunk];
-
 export default configureStore({
     reducer: rootReducer,
+    middleware: [...getDefaultMiddleware(), thunk],
+    devTools: process.env.NODE_ENV !== 'production',
     preloadedState: state
 });
