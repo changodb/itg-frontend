a8d55650ef142a9c472800ef74fc3b1c2c607b29
ryanxgraham <ryanxgraham@gmail.com>
2020-05-16 20:55:02 -0500

    cleaning up frontend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import store from './store'
 import QueryFilters from './containers/QueryFilters';
 import SimfileResults from './containers/SimfileResults';
 import Header from './components/Header';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import { spacing } from '@material-ui/system';
 
 function App() {
   return (
@@ -12,6 +18,7 @@ function App() {
 	  <div>
   		<Header />
   		<QueryFilters />
+        <Divider className="divider" />
 		<SimfileResults />
   	  </div>
 	</Provider>
--- src/components/SimfileResults.js
+++ src/components/SimfileResults.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import './styles.css';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 
 export default ({ simfileResults }) => {
     const results = simfileResults.map(
         (simfile, index) => (
-            <li key={index.toString()}>
-                {JSON.stringify(simfile)}
-            </li>)
+            <ListItem key={index.toString()}>
+                <ListItemIcon>
+                    <MusicNoteIcon />
+                </ListItemIcon>
+                {JSON.stringify(simfile, null, 1)}
+            </ListItem>)
     );
 
     return (
-        <ul>
-            {results}
-        </ul>)
+        <div className='simfileResults'>
+            <List>
+                {results}
+            </List>
+        </div>)
 }
