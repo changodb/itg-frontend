7f4f1d99b8fce50607cb291a530b8b2195a1a3ad
gene <gene@telligent-data.com>
2020-06-26 17:28:10 -0500

    explicitly setting the api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,7 @@
 import { QUERY_FIELDS } from './enums';
 
+export const API_URL = 'https://api.fitupyourstyle.com/';
+
 export const DEFAULT_QUERY_FILTER = {
   field: QUERY_FIELDS.artist,
   value: ''
