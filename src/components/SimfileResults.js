930ed7a6187e66c3103c4707a9225ed764cd797f
ryanxgraham <ryanxgraham@gmail.com>
2020-06-28 22:38:14 -0500

    added functionality to sort tables

--- src/components/SimfileResults.js
+++ src/components/SimfileResults.js
@@ -1,67 +1,140 @@
 import React, { Component } from 'react';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, List, ListItem, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, Box} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel, List, ListItem, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, Box} from '@material-ui/core';
 import _ from 'underscore';
 
 export default ({ simfileResults }) => {
 
 
-    const columns = [
-        {id: 'artist', label: 'Artist Name'},
-        {id: 'name', label: 'Track Name'},
-        {id: 'bpm', label: 'BPM'},
-        {id: ['pack', 'name'], label: 'Pack Name'},
-        {id: 'difficultyMap', label: 'Difficulties'},
-    ];
+  const columns = [
+      {id: 'artist', label: 'Artist Name'},
+      {id: 'name', label: 'Track Name'},
+      {id: 'bpm', label: 'BPM'},
+      {id: ['pack', 'name'], label: 'Pack Name'},
+      {id: 'difficultyMap', label: 'Difficulties'},
+  ];
 
-    String.prototype.toProperCase = function () {
-    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    };
-    var rows = []
-    for (let [key, value] of Object.entries(simfileResults)) {
-            console.log(`${key}: ${value}`);
-            rows.push(value)
-            for (let [inner_key, inner_value] of Object.entries(simfileResults[key])) {
-                console.log(`${inner_key}: ${inner_value}`);
-            }
+  String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  };
+  var rows = []
+  for (let [key, value] of Object.entries(simfileResults)) {
+          console.log(`${key}: ${value}`);
+          rows.push(value)
+          for (let [inner_key, inner_value] of Object.entries(simfileResults[key])) {
+              console.log(`${inner_key}: ${inner_value}`);
+          }
+  }
+//
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
     }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
 
-    function StickyHeadTable() {
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
 
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
 
-        const [page, setPage] = React.useState(0);
-        const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const headCells = [
+    {id: 'artist', disablePadding: true, label: 'Artist Name'},
+    {id: 'name', disablePadding: true,  label: 'Track Name'},
+    {id: 'bpm', disablePadding: true,  label: 'BPM'},
+    {id: ['pack', 'name'], disablePadding: true, label: 'Pack Name'},
+    {id: 'difficultyMap', disablePadding: true,  label: 'Difficulties'},
+  ];
 
-        const handleChangePage = (event, newPage) => {
-            setPage(newPage);
-        };
+  function EnhancedTableHead(props) {
+    const { order, orderBy,  onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
 
-        const handleChangeRowsPerPage = (event) => {
-            setRowsPerPage(+event.target.value);
-            setPage(0);
-        };
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              padding={headCell.disablePadding ? 'none' : 'default'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <span className='hidden'>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </span>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+
+  EnhancedTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+  };
+//
+
+    function EnhancedTable() {
+      const [order, setOrder] = React.useState('asc');
+      const [orderBy, setOrderBy] = React.useState('artist');
+      const [page, setPage] = React.useState(0);
+      const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+      const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+      };
+      const handleChangePage = (event, newPage) => {
+          setPage(newPage);
+      };
+      const handleChangeRowsPerPage = (event) => {
+          setRowsPerPage(+event.target.value);
+          setPage(0);
+      };
         return (
 
             <Box>
                 <TableContainer>
                     <Table simfileTable aria-label='simfile table'>
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) =>
-                                    <TableCell
-                                        key={column.id.toString()}
-                                        align="flex-start"
-                                    >
-                                    <Typography variant='h5'>
-                                        {column.label}
-                                    </Typography>
-
-                                    </TableCell>
-                                )}
-                            </TableRow>
-                        </TableHead>
+                        <EnhancedTableHead
+                          order={order}
+                          orderBy={orderBy}
+                          onRequestSort={handleRequestSort}
+                        />
                         <TableBody>
-                            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                          {stableSort(rows, getComparator(order, orderBy))
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row, index) => {
                               return (
                                   <TableRow hover role="checkbox" key={row.songArtist}>
                                     {columns.map((column) => {
@@ -125,7 +198,7 @@ export default ({ simfileResults }) => {
     }
     return (
         <Box>
-            <StickyHeadTable />
+            <EnhancedTable />
         </Box>
     )
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,18 +1,48 @@
 export default {
     simfileResults: [
-	    {
-		   "name":"Up",
-		   "artist":"Fit",
-		   "bpm": 'Your',
+      {
+		   "name":"My cool song",
+		   "artist":"DJ SNAZZ",
+		   "bpm":69,
 		   "pack" : {
-               "name":"Style"
+               "name":"Foobar"
            },
 		   "difficultyMap":{
-		      "easy":'Know',
-              "medium": 'Your',
-              "hard": 'Difficulty'
+		      "easy":3,
+              "medium": 5,
+              "hard": 9
 		   },
-           "difficulites": []
+           "difficulites": [
+               3, 5 , 9
+           ]
 		},
+		{
+		   "name":"Freaknik is Bacc",
+		   "artist":"T-Pain",
+		   "bpm":420,
+           "pack" : {
+               "name":"Rackem"
+           },
+		   "difficultyMap":{
+              "gucci": 8,
+              "oni":13,
+		   },
+		   "difficulties": [8, 13]
+		},
+		{
+		   "name":"Thizzle DanCE",
+		   "artist":"Mac Dre",
+		   "bpm":111,
+           "pack" : {
+               "name":"Hoozawatzit"
+           },
+		   "difficultyMap":{
+		      "beginner":1,
+		      "light":3,
+		      "standard":5,
+		      "heavy":8,
+		      "challenge":11,
+		   },
+    }
     ]
 };
