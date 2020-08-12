8b0926e24c37364ed1f502de31bad8dc2811cb92
gene <gene@telligent-data.com>
2020-08-11 19:11:33 -0500

    use node env to determine environment

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import { QUERY_FIELDS } from './enums';
 
-export const API_URL = 'https://api.fitupyourstyle.com/';
+
+export const API_URL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:3001/' : 'https://api.fitupyourstyle.com/';
 
 export const DEFAULT_QUERY_FILTER = {
   field: QUERY_FIELDS.artist,
